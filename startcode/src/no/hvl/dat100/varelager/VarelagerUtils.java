011ca4c772b5b11e1c84813c50c4325d3e29b2fc
Krismaster <594623@stud.hvl.no>
2020-09-29 14:49:54 +0200

    Finished task

--- startcode/src/no/hvl/dat100/varelager/VarelagerUtils.java
+++ startcode/src/no/hvl/dat100/varelager/VarelagerUtils.java
@@ -3,26 +3,36 @@ package no.hvl.dat100.varelager;
 public class VarelagerUtils {
 
 	public static Vare finnBilligste(Vare[] varer) {
-		
-		throw new TODO("finnBilligste");
-
+		Vare billigst = varer[0];
+		for (int i = 1; i < varer.length; i++) {
+			if (varer[i].getPris() < billigst.getPris()) {
+				billigst = varer[i];
+			}
+		}
+		return billigst;
 	}
 	
 	public static double totalPris(Vare[] varer) {		
-		
-		throw new TODO("totalPris");
-
+		double sum = 0;
+		for (int i = 0; i < varer.length; i++) {
+			sum += varer[i].getPris();
+		}
+		return sum;
 	}
 	
 	public static int[] finnVarenr(Vare[] varer) {
-				
-		throw new TODO("finnVarenr");
-
+		int[] varenr = new int[varer.length];
+		for (int i = 0; i < varer.length; i++) {
+			varenr[i] = varer[i].getVarenr();
+		}
+		return varenr;
 	}
 	
 	public static double[] finnPrisDifferanser(Vare[] varer) {
-				
-		throw new TODO("finnPrisDifferanser");
-
+		double[] differanser = new double[varer.length - 1];
+		for (int i = 0; i < differanser.length; i++) {
+			differanser[i] = varer[i + 1].getPris() - varer[i].getPris();
+		}
+		return differanser;
 	}
 }
